6cece90db1860a55c10a0944b4325937030b8b37
Macknive <kevin.corpuz@virtusize.com>
2024-04-17 20:15:50 +0800

    update

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,9 +1,24 @@
+const inpageButton = "#vs-inpage button"
+const privacyPolicyCheck = "#linkText"
+const onBoardingNext =
+  "#vs-aoyama-main-modal > div[class^='_root'] > div:nth-child(3) > div > div:nth-child(3) > button"
+const genderProceed =
+  "#vs-aoyama-main-modal > div[class^='_root'] > div:nth-child(3) > div > div:nth-child(4) > button"
+const bodyProceed =
+  "#vs-aoyama-main-modal > div[class^='_root'] > div:nth-child(3) > div > div:nth-child(5) > button"
+
 describe("Virtusize QA Automation", () => {
   before(() => {
     cy.visit("https://shop.adidas.jp/products/HR4607/")
   })
   it("check network events", () => {
-    var validProduct, sawWidget, openedWidget
+    var validProduct,
+      sawWidget,
+      openedWidget,
+      sawOnboardingScreen,
+      createdSilhouette,
+      tryItOn,
+      srapi
     cy.intercept(
       {
         method: "GET",
@@ -13,7 +28,6 @@ describe("Virtusize QA Automation", () => {
         req.reply((res) => {
           if (res.body.name === "backend-checked-product") {
             validProduct = res.body.data.validProduct
-            console.log("val", validProduct)
           }
         })
         //get response value here
@@ -22,7 +36,7 @@ describe("Virtusize QA Automation", () => {
     cy.intercept(
       {
         method: "POST",
-        url: /virtusize/,
+        url: /.*virtusize.*/,
       },
       (req) => {
         if (req.body.name === "user-saw-widget-button") {
@@ -31,20 +45,48 @@ describe("Virtusize QA Automation", () => {
         if (req.body.name === "user-opened-widget") {
           openedWidget = true
         }
-        //track events here
+        if (req.body.name === "user-saw-onboarding-screen") {
+          sawOnboardingScreen = true
+        }
+        if (req.body.name === "user-created-silhouette") {
+          createdSilhouette = true
+        }
+        if (req.body.name === "user-opened-panel-tryiton") {
+          tryItOn = true
+        }
+        //intercept response
+        req.reply((res) => {
+          if (
+            res.url.includes("size-recommendation.virtusize") &&
+            res.statusCode === 200
+          ) {
+            srapi = true
+          }
+        })
       }
     ).as("vsEvents")
     cy.wait("@vsEvents").then((interceptions) => {
-      cy.get("#vs-inpage button", { timeout: 10000 }).click()
-      // add clickables here
-      setTimeout(() => {
-        const data = {
-          isValidProduct: validProduct,
-          userSawWidget: sawWidget,
-          userOpenedWidget: openedWidget,
-        }
-        console.log(data)
-      }, 5000)
+      cy.get(inpageButton, { timeout: 20000 }).click()
+      cy.get(privacyPolicyCheck, { timeout: 5000 }).next().click()
+      cy.get(onBoardingNext, { timeout: 5000 }).click()
+      cy.get(genderProceed, { timeout: 5000 }).first().click()
+      cy.get(bodyProceed, { timeout: 5000 })
+        .first()
+        .click()
+        .then(() => {
+          setTimeout(() => {
+            const data = {
+              isValidProduct: validProduct,
+              userSawWidget: sawWidget,
+              userOpenedWidget: openedWidget,
+              userSawOnboardingScreen: sawOnboardingScreen,
+              userCreatedSilhouette: createdSilhouette,
+              userOpenedPanelTryiton: tryItOn,
+              sizeRecommendation: srapi,
+            }
+            console.log(data)
+          }, 3000)
+        })
     })
   })
 })
